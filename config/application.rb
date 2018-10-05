49309f62dd360740baff38a0566b8571b688acbf
Patrick Sinclair <metade@gmail.com>
2018-10-05 14:40:18 +0100

    Setting default time zone

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module GoodgymMissions
   class Application < Rails::Application
+    config.time_zone = "London"
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
