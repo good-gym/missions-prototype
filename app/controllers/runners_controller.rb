a8ee5f6d85f1b301c3b7416faee289cfdee85619
Patrick Sinclair <metade@gmail.com>
2018-10-05 15:08:29 +0100

    Managing runners

---
+++ app/controllers/runners_controller.rb
@@ -0,0 +1,28 @@
+class RunnersController < ApplicationController
+  def index
+    @runners = Runner.all
+  end
+
+  def show
+    @runner = Runner.find(params[:id])
+  end
+
+  def new
+    @runner = Runner.new
+  end
+
+  def create
+    @runner = Runner.new(runner_params)
+    if @runner.save
+      redirect_to runners_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def runner_params
+    params.require(:runner).permit(:name)
+  end
+end
