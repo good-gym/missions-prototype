118591235d8d26830fe7465db2ecd19fae71cf74
Patrick Sinclair <metade@gmail.com>
2018-10-11 13:13:25 +0100

    Adding README

--- README.md
+++ README.md
@@ -15,6 +15,18 @@ Things you may want to cover:
 
 * Database initialization
 
+To reset the database locally:
+
+```
+rails db:drop db:create db:setup
+```
+
+To reset the database on Heroku:
+
+```
+heroku restart; heroku pg:reset DATABASE; heroku run rake db:migrate db:seed
+```
+
 * How to run the test suite
 
 * Services (job queues, cache servers, search engines, etc.)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+times = [{ started_at: (2.week.from_now.beginning_of_week - 12.hours) }]
+
+runner = Runner.create(name: "Patrick")
+runner.availabilities
+  .create!(
+    radius: 5.0,
+    postcode_str: "E9 7HW",
+    time_slots_attributes: times
+  )
+
+runner = Runner.create(name: "Ivo")
+runner.availabilities
+  .create!(
+    radius: 5.0,
+    postcode_str: "E2 7RG",
+    time_slots_attributes: times
+  )
+
+  runner = Runner.create(name: "Polly")
+  runner.availabilities
+    .create!(
+      radius: 5.0,
+      postcode_str: "W12 7PJ",
+      time_slots_attributes: times
+    )
+
+  runner = Runner.create(name: "James")
+  runner.availabilities
+    .create!(
+      radius: 5.0,
+      postcode_str: "W11 4UL",
+      time_slots_attributes: times
+    )
+
+  runner = Runner.create(name: "Paul")
+  runner.availabilities
+    .create!(
+      radius: 5.0,
+      postcode_str: "NW1 8QP",
+      time_slots_attributes: times
+    )
+
+referrer = Referrer.create(name: "Bob")
